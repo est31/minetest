819fda702a77d275c099076af868baa40bdbaa86
est31 <MTest31@outlook.com>
2015-07-09 20:51:34 +0200

    Remove map code

--- src/script/lua_api/l_areastore.h
+++ src/script/lua_api/l_areastore.h
@@ -22,12 +22,7 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 
 #include "lua_api/l_base.h"
 #include "irr_v3d.h"
-#include <map>
 #include "areastore.h"
-
-class Map;
-class MapBlock;
-class MMVManip;
 
 /*
   AreaStore
